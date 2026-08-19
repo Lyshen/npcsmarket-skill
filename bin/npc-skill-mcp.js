1483a0c664d356a1d86fc25ecd507bbc946cdcd1
Lyshen <luoyaoshen@gmail.com>
2026-08-19 20:45:58 +0800

    Bump version to 0.2.3

--- bin/npc-skill-mcp.js
+++ bin/npc-skill-mcp.js
@@ -9,7 +9,7 @@ import { composePrompt, randomNpc } from "../src/index.js";
 const server = new McpServer(
   {
     name: "npcsmarket",
-    version: "0.2.2",
+    version: "0.2.3",
   },
   {
     instructions:
